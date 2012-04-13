b4c744d507ceff4b84f1cad4b34720d258bf3bca
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2012-04-13 12:42:10 +0530

    Completed 10 Exercises

--- pe10.py
+++ pe10.py
@@ -2,8 +2,10 @@
 
 # Find the sum of all the primes below two million.
 
+from math import sqrt
+
 def isprime(x):
-    for i in range(2, x):
+    for i in range(2, int(sqrt(x))+1):
         if x % i == 0:
             return 0
     return 1
